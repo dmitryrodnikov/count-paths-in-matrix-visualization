ddf0de8745558b8e5958c375d7b2759d6fc6546b
rodnikovdi <rodnikovdi@vtblife.ru>
2021-01-09 21:01:58 +0300

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: 'all',
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    printWidth: 120,
+};
